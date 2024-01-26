d789ce6e40ab20d84df0aefe60014dd833f97bb5
Patrick Stoeckle <patrick.stoeckle@siemens.com>
2024-01-26 16:24:44 +0000

    :sparkles: Regal devcontainer

--- test/regal/test.sh
+++ test/regal/test.sh
@@ -20,10 +20,10 @@
 #
 # These scripts are run as 'root' by default. Although that can be changed
 # with the '--remote-user' flag.
-# 
+#
 # This test can be run with the following command:
 #
-#    devcontainer features test    \ 
+#    devcontainer features test    \
 #               --features color   \
 #               --remote-user root \
 #               --skip-scenarios   \
@@ -40,7 +40,7 @@ source dev-container-features-test-lib
 # Feature-specific tests
 # The 'check' command comes from the dev-container-features-test-lib. Syntax is...
 # check <LABEL> <cmd> [args...]
-check "validate favorite color" color | grep 'my favorite color is red'
+check "regal installed" bash -c "regal version | grep '0.15.0'"
 
 # Report result
 # If any of the checks above exited with a non-zero exit code, the test will fail.
